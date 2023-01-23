82bcffcacc13e80f6c6c6e8e5a6811932b2ca79b
Harshita <harshita@docquity.com>
2023-01-23 17:39:31 +0530

    documentation for packages in go

---
+++ main.go
@@ -0,0 +1,15 @@
+/*
+package is a collection of common source code files
+
+Executable package: package main is an executable package which would produce an executable file (for main.go)
+on being compiled and hence, it should contain a function named main which is the entry point of the project
+
+Reusable package: other (non-executable) helper packages can have any name other than main
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello there!")
+}
