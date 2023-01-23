25fbaa3e47855b0054d4efdc5050706b84ac333d
Harshita <harshita@docquity.com>
2023-01-23 18:36:10 +0530

    documentation for import a package

--- main.go
+++ main.go
@@ -1,4 +1,6 @@
 /*
+	package <name>
+
 package is a collection of common source code files
 
 Executable package: package main is an executable package which would produce an executable file (for main.go)
@@ -8,6 +10,24 @@ Reusable package: other (non-executable) helper packages can have any name other
 */
 package main
 
+/*
+	Single package import:
+		import "<package name>"
+
+	Multiple packages import:
+		import (
+			"<package name>",
+			"<package name>",
+			"<package name>"
+			...
+		)
+import provides access to all functionalities of a package to the current working package
+
+fmt: is a standard go library which is used to format printing data
+Go Standard Libraries are listed here: https://pkg.go.dev/std
+
+other reusable packages can be imported in same way
+*/
 import "fmt"
 
 func main() {
