49fea5111c408ef1beabc2d210073cbb85ac695b
Harshita <harshita@docquity.com>
2023-01-24 14:56:18 +0530

    Variable declarations in Go

--- main.go
+++ main.go
@@ -3,18 +3,18 @@
 
 package is a collection of common source code files
 
-Executable package: package main is an executable package which would produce an executable file (for main.go)
+- Executable package: package main is an executable package which would produce an executable file (for main.go)
 on being compiled and hence, it should contain a function named main which is the entry point of the project
 
-Reusable package: other (non-executable) helper packages can have any name other than main
+- Reusable package: other (non-executable) helper packages can have any name other than main
 */
 package main
 
 /*
-	Single package import:
+	- Single package import:
 		import "<package name>"
 
-	Multiple packages import:
+	- Multiple packages import:
 		import (
 			"<package name>"
 			"<package name>"
@@ -24,7 +24,7 @@ package main
 import provides access to all functionalities of a package to the current working package
 
 fmt: is a standard go library which is used to format printing data
-Go Standard Libraries are listed here: https://pkg.go.dev/std
+Go Standard Libraries are listed [here]: https://pkg.go.dev/std
 
 other reusable packages can be imported in same way
 */
@@ -40,6 +40,32 @@ func is keyword to declare a function in go
 func main(): has a special purpose to be the entry point of an executable main file/ package
 */
 func main() {
-	// Using Println function from fmt package to print out text
-	fmt.Println("Hello there!")
+	/*
+		# Variable Declaration
+
+			var <variable name> <variable type> = <variable value>
+
+		var is keyword to declare a variable in Go
+
+		Go is a STATIC-TYPED language which means that variables type are known at the compile time
+		As written below, `card` variable is declared of type string. Now, `card` can only be assinged
+		a string value. Assinging any other value other than string will produce an error.
+	*/
+	var card string = "Ace of Spades"
+
+	// Using Println function from fmt package to print out value of `card`
+	fmt.Println(card)
+
+	/*
+		# Shorthand Variable Declaration
+
+			<variable name> := <variable value>
+
+		:= is the reserved symbol to declare and initialize a variable in Go. Go compiler inferes the type
+		of variable from its value.
+	*/
+	altCard := "King of Heart"
+
+	// Using Println function from fmt package to print out value of `altCard`
+	fmt.Println(altCard)
 }
